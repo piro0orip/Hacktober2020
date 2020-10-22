1422de552e6739ae864e82085e1bbe9ff521bc0e
piro0orip <73287483+piro0orip@users.noreply.github.com>
2020-10-22 16:55:27 +0530

    Add files via upload

---
+++ geodata/geodata/where.js
@@ -0,0 +1,16 @@
+myData = [
+[22.7195687,75.8577258, 'Indore, Madhya Pradesh, India'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4532165,-112.0719833, 'Phoenix, AZ 85004, USA'],
+[33.9094132,-83.4603953, '2500 Daniells Bridge Rd #300, Athens, GA 30606, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8939569,3.7187158, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9503878,77.5022224, 'Mysore Rd, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5469132,-97.1210998, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, Bei Tai Ping Zhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[53.8938988,27.5460609, 'praspiekt Niezaliežnasci 4, Minsk, Belarus'],
+[44.8184518,20.4575913, 'Studentski trg 1, Beograd, Serbia']
+];
